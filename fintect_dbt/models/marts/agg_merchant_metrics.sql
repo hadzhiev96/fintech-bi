427dfc4e85762a6f28b853ed39e221e815fd92bf
Dimitar Hadzhiev <hadzhiev96@gmail.com>
2026-06-19 18:10:31 +0300

    refactor: source net_revenue from fct_transactions and restructure agg_merchant_metrics

--- fintect_dbt/models/marts/agg_merchant_metrics.sql
+++ fintect_dbt/models/marts/agg_merchant_metrics.sql
@@ -1,34 +1,56 @@
-WITH transactions_enriched AS(
-    SELECT 
+WITH transactions AS (
+    SELECT
         merchant_key,
-        merchant_name,
-        business_category,
         interchange_fee,
         scheme_fee,
         is_chargeback,
         fraud_loss,
-        transaction_id
+        transaction_id,
+        net_revenue
 
     FROM
-        {{ref('int_transactions_enriched')}}
+        {{ ref('fct_transactions') }}
+),
+
+merchant_info AS (
+    SELECT
+        merchant_key,
+        merchant_name,
+        business_category
+    FROM
+        {{ ref('dim_merchant') }}
+),
+
+aggregated AS (
+    SELECT
+        t.merchant_key,
+        m.merchant_name,
+        m.business_category,
+        SUM(t.interchange_fee) AS total_interchange_fee,
+        SUM(t.scheme_fee) AS total_scheme_fee,
+        SUM(t.fraud_loss) AS total_fraud_loss,
+        SUM(t.net_revenue) AS net_revenue,
+        COUNT(t.transaction_id) AS total_transactions_count,
+        ROUND(
+            SUM(
+                CASE
+                    WHEN t.is_chargeback = TRUE THEN 1
+                    ELSE 0
+                END
+            )::decimal / COUNT(t.transaction_id)::decimal * 100,
+            2
+        ) AS chargeback_pct
+    FROM
+        transactions AS t
+    INNER JOIN
+        merchant_info AS m
+        ON t.merchant_key = m.merchant_key
+    GROUP BY
+        t.merchant_key,
+        m.merchant_name,
+        m.business_category
 )
 
-SELECT
-    merchant_key,
-    merchant_name,
-    business_category,
-    SUM(interchange_fee) as total_interchange_fee,
-    SUM(scheme_fee) as total_scheme_fee,
-    SUM(fraud_loss) as total_fraud_loss,
-    (SUM(interchange_fee) - SUM(scheme_fee) - SUM(fraud_loss)) AS net_revenue,
-    COUNT(transaction_id) as total_transactions_count,
-    ROUND(sum(CASE
-		WHEN is_chargeback = TRUE THEN 1
-		ELSE 0
-	END)::decimal / count(transaction_id)::decimal * 100, 2) AS chargeback_pct
-FROM 
-   transactions_enriched
-GROUP BY
-    merchant_key,
-    merchant_name,
-    business_category
+SELECT *
+FROM
+    aggregated
