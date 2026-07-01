33ab2caad00a6eda33faff497ea60fe360bb7fd6
Dimitar Hadzhiev <hadzhiev96@gmail.com>
2026-07-01 17:47:38 +0300

    feat: add year_month column to dim_date

--- fintect_dbt/models/marts/dim_date.sql
+++ fintect_dbt/models/marts/dim_date.sql
@@ -5,6 +5,7 @@ SELECT
     transaction_month,
     transaction_quarter,
     transaction_year,
+    CONCAT(transaction_year,'-',LPAD(transaction_month::text,2,'0')) AS transaction_year_month,
     is_weekend
 FROM
     {{ref('stg_dates')}}
