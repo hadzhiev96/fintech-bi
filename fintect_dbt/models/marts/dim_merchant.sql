949d64c61c69a3c19f57a61958fd9c5157535069
Dimitar Hadzhiev <hadzhiev96@gmail.com>
2026-07-09 15:45:19 +0300

    feat: add display name ( name + key ) for dim_merchant and updated power bi merchant analysis and merchant details sheets to use it

--- fintect_dbt/models/marts/dim_merchant.sql
+++ fintect_dbt/models/marts/dim_merchant.sql
@@ -1,9 +1,10 @@
 SELECT 
     merchant_key,
     merchant_name,
+    CONCAT(merchant_name, ' (', merchant_key, ')') AS merchant_display_name,
     business_category,
     city,
     country,
     bank_key
 FROM
-    {{ref('stg_merchants')}}
+    {{ ref('stg_merchants') }}
